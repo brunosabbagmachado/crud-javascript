1897151a858f1b0a5f5ce4d294b4fee61590bf1f
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-11-21 20:07:46 -0300

    modulo 2

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,32 +1,34 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCLiente = document.createElement('tr');
-    const conteudo = `
-        <td class="td" data-td>${nome}</td>
-        <td>${email}</td>
-        <td>
-            <ul class="tabela__botoes-controle">
-                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-            </ul>
-        </td>
-    `
-    linhaNovoCLiente.innerHTML = conteudo;
-    return linhaNovoCLiente;
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
+            return resposta.json();
+        })
 }
 
-const tabela = document.querySelector('[data-tabela]');
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        return resposta.body;
+    })
+}
 
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
 
-
-const http = new XMLHttpRequest();
-
-http.open('GET', 'http://localhost:3000/profile');
-
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response);
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
-    });
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente
 }
