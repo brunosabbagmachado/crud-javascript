c5a0e75049ddd6ff4523f626c9c27f52587999d9
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-11-20 23:29:58 -0300

    criando template

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,3 +1,23 @@
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCLiente = document.createElement('tr');
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td>
+    `
+    linhaNovoCLiente.innerHTML = conteudo;
+    return linhaNovoCLiente;
+}
+
+const tabela = document.querySelector('[data-tabela]');
+
+
+
 const http = new XMLHttpRequest();
 
 http.open('GET', 'http://localhost:3000/profile');
@@ -5,6 +25,8 @@ http.open('GET', 'http://localhost:3000/profile');
 http.send();
 
 http.onload = () => {
-    const data = http.response;
-    console.log(data);
+    const data = JSON.parse(http.response);
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+    });
 }
